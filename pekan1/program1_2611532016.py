ddd5f71ab640310058e8daa2e3ed21536c662e9e
addhiee <ahmadaffandi317@gmail.com>
2026-09-03 11:47:43 +0700

    pekan 1 pratikum kelas C

---
+++ pekan1/program1_2611532016.py
@@ -0,0 +1,13 @@
+# program pertama
+""""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("HEllo world")
+print()
+print("Departemen Informatika")
+print("Universitas Andalas")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + " dibuat oleh a\\n "
++ "gilang adhatul sukri 2611532016")
